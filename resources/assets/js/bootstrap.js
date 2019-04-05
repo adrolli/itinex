71b59f37b6eb80f23ed207ce8680b18b1f1fe3ec
Alf Drollinger <alf@drollinger.info>
2019-04-05 21:57:26 +0700

    add bulma, remove bs
    Change from Bootstrap to Bulma

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -11,7 +11,8 @@ window.Popper = require('popper.js').default;
 try {
     window.$ = window.jQuery = require('jquery');
 
-    require('bootstrap');
+// Use bulma without JS, no bootstrap
+//    require('bootstrap');
 } catch (e) {}
 
 /**
